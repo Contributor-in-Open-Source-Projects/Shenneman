58b21ec5a4eb8b6b592c5ed2e3d323babe9b84a7
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-04-12 20:30:35 +0500

    INFRA: Initialize Api Project

---
+++ Shenneman/Shenneman.Api/Controller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfrom and Peace
+//==================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Shenneman.Api.Controller
+{
+    [ApiController]
+    [Route("api/Controller")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello World");  
+      
+    }
+}
---
+++ Shenneman/Shenneman.Api/Startup.cs
@@ -0,0 +1,60 @@
+//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfrom and Peace
+//==================================================
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Shenneman.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiInfo = new OpenApiInfo()
+            {
+                Title = "Shenneman.Api",
+                Version = "v1"
+            };
+
+            services.AddControllers();
+            services.AddSwaggerGen(Options =>
+            {
+                Options.SwaggerDoc(
+                    name:"v1",
+                   info: apiInfo);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment Enveriment)
+        {
+            if (Enveriment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(Config => Config.SwaggerEndpoint(
+                   url: "/swagger/v1/swagger.json",
+                   name: "Shenneman.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+    }
+}
