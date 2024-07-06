2d6943f85b5ef1f64baab15b2648c3010d99e31d
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-07-06 19:03:47 +0500

    sdff

--- Shenneman.Api/Program.cs
+++ Shenneman.Api/Program.cs
@@ -5,7 +5,7 @@
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-                //  code rub 
+                //  code rub hjh
 namespace Shenneman.Api
 {
     public class Program
